021a78d832d44ea859829fb6f9c530f57dde29cd
eghearn <eghearn05@gmail.com>
2023-09-14 22:54:19 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+            double springCost = 1300.50;
+            double summerCost = 2590.89;
+            double fallCost = 1876.33;
+            double winterCost = 4782.98;
+            double yearlyCost = springCost + summerCost + fallCost + winterCost;
+            System.out.println("Your yearly maintenance cost is " + springCost + ", " + summerCost + ", " + fallCost + ", " + winterCost + " is " + yearlyCost);
+    }
+}
